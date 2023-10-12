f720b93c276ac635162a4c0379df318d1039f082
georgeno1ukno <201020588@ingenieria.usac.edu.gt>
2023-10-11 20:49:10 -0600

    JS practice

---
+++ non_blocking.js
@@ -0,0 +1,5 @@
+console.log("I’m learning about");
+setTimeout(() => {
+  console.log("Event Loop");
+}, 2000);
+console.log("the");
---
+++ water-limits.js
@@ -0,0 +1,15 @@
+/* water-limits.js */
+/* function celsiusToFahrenheit(celsius) {
+  return celsius * (9 / 5) + 32;
+}
+ */
+const converters = require("./converters.js");
+
+const freezingPointC = 0;
+const boilingPointC = 100;
+
+const freezingPointF = converters.celsiusToFahrenheit(freezingPointC);
+const boilingPointF = converters.celsiusToFahrenheit(boilingPointC);
+
+console.log(`The freezing point of water in Fahrenheit is ${freezingPointF}`);
+console.log(`The boiling point of water in Fahrenheit is ${boilingPointF}`);
